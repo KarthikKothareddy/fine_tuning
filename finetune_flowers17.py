b6edfe7aada8caab6e5b348ed8d511c2b7b193dc
cn180450 <karthikreddy.kp@gmail.com>
2019-07-20 21:56:07 -0500

    paramenter tuning

--- finetune_flowers17.py
+++ finetune_flowers17.py
@@ -32,9 +32,15 @@ ap.add_argument("-m", "--model", required=True,
 args = vars(ap.parse_args())
 
 # construct the image generator for data augmentation
-aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
-	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
-	horizontal_flip=True, fill_mode="nearest")
+aug = ImageDataGenerator(
+	rotation_range=30,
+	width_shift_range=0.1,
+	height_shift_range=0.1,
+	shear_range=0.2,
+	zoom_range=0.2,
+	horizontal_flip=True,
+	fill_mode="nearest"
+)
 
 # grab the list of images that we'll be describing, then extract
 # the class label names from the image paths
@@ -55,17 +61,28 @@ data = data.astype("float") / 255.0
 
 # partition the data into training and testing splits using 75% of
 # the data for training and the remaining 25% for testing
-(trainX, testX, trainY, testY) = train_test_split(data, labels,
-	test_size=0.25, random_state=42)
+(trainX, testX, trainY, testY) = train_test_split(
+	data, labels,
+	test_size=0.25,
+	random_state=42
+)
+
+print("Before lables: {}".format(trainY.shape))
 
 # convert the labels from integers to vectors
 trainY = LabelBinarizer().fit_transform(trainY)
 testY = LabelBinarizer().fit_transform(testY)
 
+print("After lables: {}".format(trainY.shape))
+
+
 # load the VGG16 network, ensuring the head FC layer sets are left
 # off
-baseModel = VGG16(weights="imagenet", include_top=False,
-	input_tensor=Input(shape=(224, 224, 3)))
+baseModel = VGG16(
+	weights="imagenet",
+	include_top=False,
+	input_tensor=Input(shape=(224, 224, 3))
+)
 
 # initialize the new head of the network, a set of FC layers
 # followed by a softmax classifier
@@ -84,17 +101,24 @@ for layer in baseModel.layers:
 # layers to being non-trainable
 print("[INFO] compiling model...")
 opt = RMSprop(lr=0.001)
-model.compile(loss="categorical_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+model.compile(
+	loss="categorical_crossentropy",
+	optimizer=opt,
+	metrics=["accuracy"]
+)
 
 # train the head of the network for a few epochs (all other
 # layers are frozen) -- this will allow the new FC layers to
 # start to become initialized with actual "learned" values
 # versus pure random
 print("[INFO] training head...")
-model.fit_generator(aug.flow(trainX, trainY, batch_size=32),
-	validation_data=(testX, testY), epochs=25,
-	steps_per_epoch=len(trainX) // 32, verbose=1)
+model.fit_generator(
+	aug.flow(trainX, trainY, batch_size=32),
+	validation_data=(testX, testY),
+	epochs=25,
+	steps_per_epoch=len(trainX) // 32,
+	verbose=1
+)
 
 # evaluate the network after initialization
 print("[INFO] evaluating after initialization...")
@@ -111,15 +135,22 @@ for layer in baseModel.layers[15:]:
 # the model, this time using SGD with a *very* small learning rate
 print("[INFO] re-compiling model...")
 opt = SGD(lr=0.001)
-model.compile(loss="categorical_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+model.compile(
+	loss="categorical_crossentropy",
+	optimizer=opt,
+	metrics=["accuracy"]
+)
 
 # train the model again, this time fine-tuning *both* the final set
 # of CONV layers along with our set of FC layers
 print("[INFO] fine-tuning model...")
-model.fit_generator(aug.flow(trainX, trainY, batch_size=32),
-	validation_data=(testX, testY), epochs=100,
-	steps_per_epoch=len(trainX) // 32, verbose=1)
+model.fit_generator(
+	aug.flow(trainX, trainY, batch_size=32),
+	validation_data=(testX, testY),
+	epochs=100,
+	steps_per_epoch=len(trainX) // 32,
+	verbose=1
+)
 
 # evaluate the network on the fine-tuned model
 print("[INFO] evaluating after fine-tuning...")
@@ -129,4 +160,5 @@ print(classification_report(testY.argmax(axis=1),
 
 # save the model to disk
 print("[INFO] serializing model...")
+
 model.save(args["model"])
